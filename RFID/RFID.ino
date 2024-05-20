53802dc7b9d6cf625ead40fb2fe0b50df4a8b5b2
GalacticCodeGambit <150372421+GalacticCodeGambit@users.noreply.github.com>
2024-05-20 22:31:08 +0200

    Vor delay entfernen

--- RFID/RFID.ino
+++ RFID/RFID.ino
@@ -9,11 +9,17 @@ long chipUID;
 MFRC522 mfrc522(SS_PIN, RST_PIN); // MFRC522-Instanz erstellen
 MFRC522::MIFARE_Key key;
 
-int d = 100;
-int blockNum = 2;      // Nummer des auszulesenden Datenblockes
-byte bufferLen = 18;
+byte d = 100;
+byte blockNum = 2;      // Nummer des auszulesenden Datenblockes
 byte readData[18];
-String Data;           // ausgelesene Daten
+byte size = sizeof(readData);
+
+byte keyData[16]    = {
+  0x20, 0x20, 0x20, 0x20, //  1,  2,   3,  4,
+  0x20, 0x20, 0x20, 0x20, //  5,  6,   7,  8,
+  0x20, 0x20, 0x20, 0x20, //  9, 10, 255, 11,
+  0x20, 0x20, 0xfe, 0x3f  // 12, 13, 14, 15
+};
 
 MFRC522::StatusCode status;
 
@@ -43,7 +49,7 @@ void Read(int blockNum, byte readData[]) {
     Serial.println("Authentication success");
     delay(d);
   }
-  status = mfrc522.MIFARE_Read(blockNum, readData, &bufferLen);
+  status = mfrc522.MIFARE_Read(blockNum, readData, &size);
   if (status != MFRC522::STATUS_OK) {
     Serial.print("Reading failed: ");
     delay(d);
@@ -57,14 +63,12 @@ void Read(int blockNum, byte readData[]) {
 
 
 void loop() {
-  Data = ""; // resten von Data
-  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF; //Key festlegen 
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF; //Key festlegen
   // Sobald ein Chip aufgelegt wird startet diese Abfrage
   if (mfrc522.PICC_IsNewCardPresent()) {
-    chipUID = 0; // CardID resetten
-
     mfrc522.PICC_ReadCardSerial();
 
+    chipUID = 0; // CardID resetten
     // die ID des Chips in die Variable chipUID geladen
     for (byte i = 0; i < mfrc522.uid.size; i++) {
       chipUID = ((chipUID + mfrc522.uid.uidByte[i]) * 10);
@@ -81,14 +85,27 @@ void loop() {
     if (status == MFRC522::STATUS_OK) {
       Serial.println("Data in Block " + String(blockNum) + ":");
       Serial.print(" --> ");
-      for (int i = 0; i < 16; i++) {  //uint8_t
+      for (byte i = 0; i < 16; i++) {  
         Serial.print(readData[i] < 10 & 0x10 ? " 0" : " ");
         Serial.print(readData[i], HEX);           // Umwandeln in Hexadezimal
-        Data = Data + String(readData[i], HEX);   // Ausgelesene Daten im Block Speichern
       }
-      Serial.print("\n");
+      Serial.println();
+
+      //Checkt auf richtige Daten auf Karte
+      Serial.println(F("Checkt Karte..."));
+      byte count = 0;
+      for (byte i = 0; i < 16; i++) {
+        // Vergleicht readData mit keyData
+        if (readData[i] == keyData[i])
+          count++;
+      }
+      if (count == 16) {
+        Serial.println(F("Entriegelt :-)"));
+      } else {
+        Serial.println(F("Falsche RFID-Karte :-("));
+      }
+      Serial.println();
     }
-    Serial.println(Data); // Test
 
     //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));   // Test Dump alle Daten auf RFID-Karte
     mfrc522.PICC_HaltA();
